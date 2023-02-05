f97a14a47ca8b2b06db622f483403cd04ff0311a
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-02-05 21:28:27 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React ,{useState, useRef} from 'react'
-import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
+import React, { useState, useRef } from "react";
+import "../styles/App.css";
+import PortalButton from "./PortalButton.js";
+import PortalTextArea from "./PortalTextArea.js";
 
-//complete the function 
+//complete the function
 const App = () => {
-
-
+  const inputRef = useRef(null);
+  const [val, setVal] = useState("");
+  const changeText = () => {
+    setVal(inputRef.current.value);
+    inputRef.current.value = "";
+  };
 
   return (
-    
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input id="input" ref={inputRef}></input>
+      <br />
+      <PortalButton buttonclick={changeText} />
+      <br />
+      <PortalTextArea value={val} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/PortalTextArea.js
+++ src/components/PortalTextArea.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //complete this function using portals
-const PortalTextArea=()=>{
-    return (
-        <div>
-            <textarea id="textarea" value={}></textarea>
-        </div>
-    )
-}
+const PortalTextArea = ({ value }) => {
+  return ReactDOM.createPortal(
+    <div>
+      <textarea id="textarea" value={value}></textarea>
+    </div>,
+    document.getElementById("portal-textarea")
+  );
+};
 export default PortalTextArea;
 //portal-textarea id will be used here for portal purpose
